ac98ac20ec769c85facc0568c8e56538990fd46c
IvanCastaneda6 <ivancastanedacastillejo6@gmail.com>
2023-10-05 10:46:29 +0200

    commit

---
+++ Helloword.java
@@ -0,0 +1,7 @@
+public class Helloword {
+    public static void main(String[] args)  {
+
+        System.out.println("Hola Mundo!");
+        System.out.println("Que tengas un buen día.");
+    }    
+}
